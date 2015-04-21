ab31257f0d78273b2625cacbf36a36d0791fd9c8
Jonathanbruenderman <jonathan.bruenderman@infospace.com>
2015-04-21 15:40:44 -0700

    first commit

---
+++ Web.portfolio1/Controllers/HomeController.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace Web.portfolio1.Controllers
+{
+    public class HomeController : Controller
+    {
+        // GET: Home
+        public string Index()
+        {
+            return "This is my default action";
+        }
+
+        public ActionResult MainPage()
+        {
+            return View();
+        }
+
+        public ActionResult Projects()
+        {
+            return View();
+        }
+        public ActionResult Resume()
+        {
+            return View();
+        }
+        public ActionResult Apps()
+        {
+            return View();
+        }
+        public ActionResult ContactMe()
+        {
+            return View();
+        }
+        public ActionResult PastJobs()
+        {
+            return View();
+        }
+
+    }
+}
---
+++ Web.portfolio1/obj/Release/Package/PackageTmp/Content/Scripts/Menubar.js
@@ -0,0 +1,10 @@
+﻿
+$(document).ready(function () {
+    $('#menu-image1').click(function () {
+        $('#menu').stop().slideToggle(570);
+    });
+    $('#menu-image').click(function () {
+        $('#menu').stop().slideToggle(570);
+    });
+});
+
