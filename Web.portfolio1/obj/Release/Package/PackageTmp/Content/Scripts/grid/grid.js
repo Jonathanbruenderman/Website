71222668329e96c33671df9291d53ebe2abe7dc8
Jonathanbruenderman <jonathan.bruenderman@infospace.com>
2015-07-25 12:57:29 -0700

    Fixing styles in resume

---
+++ Web.portfolio1/obj/Release/Package/PackageTmp/Content/Scripts/grid/grid.js
@@ -0,0 +1,19 @@
+﻿angular.module('Grid', [])
+.service('GridService', function () {
+    this.grid = [];
+    this.tiles = [];
+    // Size of the board
+    this.size = 4;
+  
+});
+
+angular.module('Grid')
+.factory('TileModel', function () {
+    var Tile = function (pos, val) {
+        this.x = pos.x;
+        this.y = pos.y;
+        this.value = val || 2;
+    };
+
+    return Tile;
+})
